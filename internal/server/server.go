2b50f8501f604a2457e485c13a38c90207fa552d
Evgeny K <77lm@mail.ru>
2023-02-06 17:48:36 +0600

    test

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,21 +1,15 @@
 package server
 
 import (
-	"context"
-	"crypto/tls"
 	"errors"
 	"fmt"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	graph2 "test-server-go/graph"
+	"test-server-go/graph"
 	"test-server-go/internal/config"
 	"test-server-go/internal/mailer"
 	"test-server-go/internal/postgres"
-	"time"
 )
 
 type Application struct {
@@ -27,50 +21,23 @@ type Application struct {
 }
 
 func (app *Application) ServerRun() error {
-
-	apiGraphql := handler.NewDefaultServer(graph2.NewExecutableSchema(graph2.Config{Resolvers: &graph2.Resolver{}}))
-	http.Handle("/query", apiGraphql)
-
-	tlsConfig := &tls.Config{
-		CurvePreferences: []tls.CurveID{tls.X25519, tls.CurveP256},
-	}
-
-	srv := &http.Server{
-		Addr:         app.Config.GetURL(),
-		Handler:      apiGraphql,
-		IdleTimeout:  time.Minute,
-		ReadTimeout:  10 * time.Second,
-		WriteTimeout: 30 * time.Second,
-		TLSConfig:    tlsConfig,
-	}
-
-	shutdownError := make(chan error)
-
-	go func() {
-		quit := make(chan os.Signal, 1)
-		signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-		<-quit
-
-		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-		defer cancel()
-
-		shutdownError <- srv.Shutdown(ctx)
-	}()
+	srv := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	http.Handle("/query", srv)
 
 	if app.Config.App.DebugMode == true {
 		fmt.Println("server is running in debug mode")
 		http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-		err := srv.ListenAndServe()
+		err := http.ListenAndServe(app.Config.GetURL(), nil)
 		if !errors.Is(err, http.ErrServerClosed) {
 			return err
 		}
 	} else {
 		fmt.Println("server is running in tls mode")
-		err := srv.ListenAndServeTLS(app.Config.Tls.CertFile, app.Config.Tls.KeyFile)
+		err := http.ListenAndServeTLS(app.Config.GetURL(), app.Config.Tls.CertFile, app.Config.Tls.KeyFile, nil)
 		if !errors.Is(err, http.ErrServerClosed) {
 			return err
 		}
 	}
 
-	return <-shutdownError
+	return nil
 }
